4ce8d987880520ff1ef2b1659c208f328506b90e
abhishekabhiRaj <bcoder48@gmail.com>
2024-09-16 15:22:21 +0530

    vehicle type

--- parking.client/src/application/components/PageTitle/PageTitle.js
+++ parking.client/src/application/components/PageTitle/PageTitle.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const PageTitle = ({ handleModal, isOnlyTitle }) => {
+const PageTitle = ({ title, handleModal, isOnlyTitle }) => {
     return (<div className="d-flex align-items-center justify-content-between">
-        <p className="m-0 fw-semibold">Manage Parking Type</p>
+        <p className="m-0 fw-semibold">{title}</p>
         {!isOnlyTitle && <Button className="btn-sm" onClick={handleModal}>
             Add New
         </Button>}
